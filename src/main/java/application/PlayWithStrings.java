537234aa6fd8006ebaed7638b8207b6290b2abb0
Rosamata22 <rosamatapavia@gmail.com>
2023-11-24 16:32:05 +0100

    Playing with strings ready to be checked

--- src/main/java/application/PlayWithStrings.java
+++ src/main/java/application/PlayWithStrings.java
@@ -1,8 +1,18 @@
 package application;
+import java.util.Arrays;
+
 
 public class PlayWithStrings {
+
 	public static void main(String[] args) {
-		System.out.println("Hello String World");
+		String vorname = "Rodríguez";
+		String nachname = "González";
+		String a = "monja";
+		String b = "jamon";
+		String c = "acurruca";
+		printIntitials(vorname, nachname);
+		System.out.println("Anagrame: " + isAnagram(a,b));
+		System.out.println("Palindrome: " + isPalindrome(c));
 	}
 
 	/**
@@ -14,7 +24,7 @@ public class PlayWithStrings {
 	 * @param lastname
 	 */
 	public static void printIntitials(String firstname, String lastname) {
-
+	System.out.println("first initial: %s, last initial: %s".formatted(firstname.substring(0,1),lastname.substring(0,1)));
 	}
 
 	/**
@@ -30,9 +40,25 @@ public class PlayWithStrings {
 	 * @param value2
 	 * @return true if the values are anagrams, false otherwise.
 	 */
-	public static boolean areAnagrams(String value1, String value2) {
+	public static boolean isAnagram(String value1,String value2) {
+		String low1 = value1.toLowerCase();
+		String low2 = value2.toLowerCase();
+		String cleanlow1 = low1.replaceAll(" ", "");
+		String cleanlow2 = low2.replaceAll(" ", "");
+		char[] arr1 = new char[low1.length()];
+		arr1 = cleanlow1.toCharArray();
+		char[] arr2 = new char[low2.length()];
+		arr2 = cleanlow2.toCharArray();
+		Arrays.sort(arr1, 0, arr1.length);
+		Arrays.sort(arr2, 0, arr2.length);
+
+		if (Arrays.equals(arr1, arr2)) {
+			return true;
+		}
 		return false;
 	}
+
+
 
 	/**
 	 * TODO: palindrone checker
@@ -48,7 +74,30 @@ public class PlayWithStrings {
 	 * @param value
 	 * @return true if it is a palindrome, false otherwise.
 	 */
-	public static boolean isPalindrome(String value) {
+	public static boolean isPalindrome(String value3) {
+		char[] arr3 = new char[value3.length()];
+		arr3 = value3.toCharArray();
+		StringBuilder reversed = new StringBuilder();
+		for (int i = arr3.length; i > 0; i--) {
+			reversed.append(arr3[i - 1]);
+		}
+		String s = reversed.toString();
+		char[] arr4 = new char[s.length()];
+		arr4 = s.toCharArray();
+		/** DIFERENTES MANERAS DE INVERTIR LOS VALORES DE UN ARRAY O STRING
+		*otro metodo: StringBuilder builder = new StringBuilder(value).reverse();
+		*       return value.equals(builder.toString());
+        * otro metodo para reverse:arr4 = reversechararray(arr3);
+		* otro metodo para reverse: public static char[] reverseCharArray(char[] array) {
+		 char[] newArray = new char[array.length];
+		 for(int i = 0; i < newArray.length; i++) {
+		 newArray[i] = array[newArray.length - 1 - i];
+		 }
+		 return newArray;
+		 */
+		if (Arrays.equals(arr4, arr3)) {
+			return true;
+		}
 		return false;
 	}
 }
